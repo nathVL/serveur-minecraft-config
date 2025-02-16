d789af788582bff21691161b124df8a6af190cbf
Nathan <nathanvillette77@gmail.com>
2025-02-16 21:44:11 +0100

    Reécriture du script de backup + modification des settings du serv

--- script.sh
+++ script.sh
@@ -1,34 +1,54 @@
 #!/bin/bash
 
-chemin_backup="/mnt/usb/backup"
+if [ "$EUID" -ne 0 ]; then
+    echo "❌ Ce script doit être exécuté en tant que root (utilisez sudo)." >&2
+    exit 1
+fi
 
-nom_backup="$(date '+%d-%m-%Y_%H-%M-%S')"
-toBackup="/home/nathan/paper-mc/data/world /home/nathan/paper-mc/data/world_nether /home/nathan/paper-mc/data/world_the_end"
+BACKUP_PATH="/mnt/usb/backup/test"
+BACKUP_NAME="$(date '+%d-%m-%Y_%H-%M-%S')"
+REPOSITORY_TO_BACKUP="/home/nathan/paper-mc/data/server.properties /home/nathan/paper-mc/data/config /home/nathan/paper-mc/data/world_nether /home/nathan/paper-mc/data/worldold /home/nathan/paper-mc/data/world_the_end /home/nathan/paper-mc/data/world"
 
-if [ ! -d "$chemin_backup" ]; then
-    mkdir -p "$chemin_backup/1"
+CONTAINER_NAME="minecraft-server"
+RCON_PASSWORD="rC0npassword"
+
+# Créer un dossier si il n'y a pas encore de backup
+if [ ! -d "$BACKUP_PATH" ]; then
+    mkdir -p "$BACKUP_PATH/1"
 fi
 
 # Renommer le dossier
-nom_dossier=$(find "$chemin_backup" -mindepth 1 -maxdepth 1 -type d)
-mv "$nom_dossier" "$chemin_backup/$nom_backup"
-echo "--Debut de la sauvegarde dans $chemin_backup/$nom_backup--"
+nom_dossier=$(find "$BACKUP_PATH" -mindepth 1 -maxdepth 1 -type d)
+mv "$nom_dossier" "$BACKUP_PATH/$BACKUP_NAME"
+echo "--Debut de la sauvegarde dans $BACKUP_PATH/$BACKUP_NAME--"
 
-# echo "Fermeture du serveur"
 
+# Fermeture du serveur
+docker exec -it minecraft-server rcon-cli --password $RCON_PASSWORD "say Le serveur va faire une backup"
+docker exec -it minecraft-server rcon-cli --password $RCON_PASSWORD "say Fermeture dans 5 secondes"
+for i in {4..1}; do
+    docker exec -it minecraft-server rcon-cli --password $RCON_PASSWORD "say $i..."
+    sleep 1
+done
+docker exec -it minecraft-server rcon-cli --password $RCON_PASSWORD "say Fermeture"
+sleep 1
+docker compose down
 
 
 function backupDirectory() {
-    chemin_destination="$chemin_backup/$nom_backup"
-    cp -a "$1" "$chemin_destination"
+    # Backup le dossier donné
+    chemin_destination="$BACKUP_PATH/$BACKUP_NAME"
+    rsync -a --info=progress2 "$1" "$chemin_destination"
 }
 
-for rep in $toBackup; do
+# Backup tout les dossiers
+for rep in $REPOSITORY_TO_BACKUP; do
     echo "Sauvegarde de $rep"
     backupDirectory "$rep"
 done
 
-
-# echo "Ouverture du serveur"
+# Reouvrir le serveur
+echo "Ouverture du serveur"
+docker compose up -d
 
 echo "--Fin--"
